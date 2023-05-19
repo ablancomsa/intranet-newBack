4508768044d246bb8f287b0844c0c599de861b66
msa developer <ablanco@msaconsultores.com>
2023-05-19 16:09:57 -0400

    [ADD] metodo para enviar la solicitud

---
+++ utils/sendContact.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+const randomUseragent = require('random-useragent');
+
+const sendContact = async (userData) => {
+  
+  const header = randomUseragent.getRandom((ua) => {
+    return ua.browserName === 'Firefox';
+  });
+  console.log(header)
+  
+  browser = await puppeteer.launch({
+    headless: false,
+    ignoreHTTPSErrors: true,
+    
+  });
+  const page = await browser.newPage();
+  await page.goto('https://www.linkedin.com/login');
+  await page.type('#username', 'msadeveloper@yahoo.com');
+  await page.type('#password', 'proyectolinkedin');
+  await page.waitForTimeout(3000);
+  await page.click('button[data-litms-control-urn="login-submit"]');
+  await page.waitForTimeout(15000);
+  console.log(userData.linkedin)
+  await page.goto(`https://${userData.linkedin}`);
+  await page.waitForTimeout(3000);
+  await page.waitForSelector(`.pv-top-card-v2-ctas >>> button`);
+  const div = await page.$(`.pv-top-card-v2-ctas >>> button`);
+  console.log(div);
+  await page.click(`.pv-top-card-v2-ctas >>> button`);
+  await page.waitForTimeout(3000);
+  await page.close()
+
+  return true
+}
+
+module.exports = sendContact;
