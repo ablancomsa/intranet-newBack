89a8b54a126cd8640c4064caa5cafa2b79066657
msa developer <ablanco@msaconsultores.com>
2023-05-19 16:10:39 -0400

    [ADD] metodo para actualizar el contacto

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,7 @@ const middleware = require('../utils/middleware');
 const User = require('../models/user');
 const Company = require('../models/company');
 const getNewUsers = require('../utils/helper');
+const sendContact = require('../utils/sendContact');
 
 userRouter.get('/', async (request, response) => {
   const users = await User.find({}).populate('companySimpleText', {name: 1});
@@ -49,7 +50,8 @@ userRouter.post('/', middleware.userExtractor, async (request, response) => {
     company: person.company,
     companySimpleText: company._id,
     roleCategory: "x",
-    imgUrl: person.imgUrl || "../../../assets/user.png"
+    imgUrl: person.imgUrl || "../../../assets/user.png",
+    isCotact: false
   })
   
   
@@ -61,12 +63,49 @@ userRouter.post('/', middleware.userExtractor, async (request, response) => {
   response.json(savedUser);
 })
 
+userRouter.post('/:id', async (request, response) => {
+  const id = request.params.id;
+  const person = await User.findById(id);
+  
+
+  const newUserToAdd = {
+    name: person.name,
+    status: person.status,
+    description: person.description,
+    email: person.email,
+    phone: person.phone || 0,
+    linkedin: person.linkedin,
+    twitter: person.twitter || "x",
+    website: person.website || "x",
+    role: person.role || "x",
+    university: person.university,
+    company: person.company,
+    roleCategory: "x",
+    imgUrl: person.imgUrl,
+    isContact: true
+  }
+
+  console.log("person: ",person)
+  console.log("new user to add: ",newUserToAdd);
+
+  try {
+    await sendContact(person);
+    await User.findByIdAndUpdate(request.params.id, {$set: newUserToAdd}, {new: true});
+    response.status(200).json(newUserToAdd);
+  }
+  catch (error) {
+    console.log(error)
+   response.status(500).json({error});
+  }
+})
+
+
 userRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
   console.log("éntro delete");
   const id = request.params.id;
   console.log(id);
   await User.findByIdAndDelete(id);
-  response.status(204).json({message: "User deleted"}).end();
+  response.status(204).json({message: "User deleted"});
 })
 
 module.exports = userRouter;
